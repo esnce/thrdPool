04b1382c940ddeefa1cd760f5ce569c96075e539
Gang Yin <name.yin.gang@gmail.com>
2014-02-24 18:30:35 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,38 @@
+/* 
+ * gcc main.c thrdpool.c -lpthread 
+ * */
+
+#include "thrdPool.h" 
+
+
+void *process (void *arg); 
+
+int main(void) 
+{ 
+    char    arg[128]; 
+
+    pool_init(20);   
+    pool_add_worker(process,"1"); 
+    pool_add_worker(process,"2"); 
+    pool_add_worker(process,"3"); 
+    pool_add_worker(process,"4"); 
+    pool_add_worker(process,"5"); 
+    pool_add_worker(process,"6"); 
+    pool_add_worker(process,"7"); 
+    pool_add_worker(process,"8"); 
+    pool_add_worker(process,"9"); 
+    pool_add_worker(process,"10"); 
+    pool_add_worker(process,"11"); 
+    pool_add_worker(process,"12"); 
+    pool_add_worker(process,"13"); 
+
+    sleep(1); 
+    pool_destroy(); 
+
+    return 0; 
+} 
+
+void *process (void *arg) 
+{ 
+    printf("hello ==> %s\n",(char *)arg); 
+}
